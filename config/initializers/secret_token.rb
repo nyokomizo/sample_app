fa2d79708cd0885ca9b251144802ec2864023af5
yokomizo <yokomizo@cookpad.com>
2012-03-14 18:32:18 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e92c116a8db32dcb142e88b94aaa9cbf083ff8d6ebf900b88f6c1ce8a25a01a1b346f286de87dc3b9cd657478ac767375504c194b24658dab9d3ad6a2f6b7b7b'
